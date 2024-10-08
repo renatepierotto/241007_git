25f8fff443670b23a544bf8d17f56a09f34d78cd
renate <renatepierotto@gmail.com>
2024-10-08 13:44:09 +0100

    Add password checker and present modules, update .gitignore

---
+++ lib/present.py
@@ -0,0 +1,16 @@
+
+class Present:
+    def __init__(self):
+        self.contents = None
+
+    def wrap(self, contents):
+        if self.contents is not None:
+            self.contents = contents
+            raise Exception("A contents has already been wrapped.")
+        self.contents = contents
+
+    def unwrap(self):
+        if self.contents is None:
+            raise Exception("No contents have been wrapped.")
+        return self.contents
+
